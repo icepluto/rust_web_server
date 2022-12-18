0fa200f2c1906f07a6b0dbd7e384db9aa2c9cf60
lihao <18792689010@163.com>
2022-12-18 18:15:05 +0800

    like route

--- src/main.rs
+++ src/main.rs
@@ -15,18 +15,22 @@ fn main() {
 fn handle_connection(mut stream: TcpStream) {
     let mut buffer = [0; 1024];
     stream.read(&mut buffer).unwrap();
-    // println!("Request:{}",String::from_utf8_lossy(&buffer[..]));
 
-    let index_page = fs::read_to_string("hello.html").unwrap();
-    // println!("{:?}",page);
+    let get = b"GET / HTTP/1.1\r\n";
+    let (status_line, filname) = if buffer.starts_with(get) {
+        ("HTTP/1.1 200 OK", "hello.html")
+    } else {
+        ("HTTP/1.1 404 NOT FOUND", "error.html")
+    };
+
+    let index_page = fs::read_to_string(filname).unwrap();
 
     let response = format!(
-        "HTTP/1.1 200 OK\r\nContent-Length:{}\r\n\r\n{}",
+        "{}\r\nContent-Length:{}\r\n\r\n{}",
+        status_line,
         index_page.len(),
         index_page
     );
-    // let res_byte = response.as_bytes();
-    // println!("{:?}",res_byte);
     stream.write(response.as_bytes()).unwrap();
     stream.flush().unwrap();
 }
